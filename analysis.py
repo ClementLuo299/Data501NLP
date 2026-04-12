08a7f579d60040a7d0f16b852ccf1225a27591b7
Clement Luo <clementluo299@gmail.com>
2026-04-11 19:45:06 -0600

    added the rest of the code

---
+++ analysis.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from scipy import sparse
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
+
+# Load data
+X_tfidf = np.load("clinicalbert_x.npy")
+y = np.load("y.npy", allow_pickle=True)
+
+# Split into train and test data
+X_train, X_test, y_train, y_test = train_test_split(
+    X_tfidf, y, test_size=0.15, stratify=y
+)
+
+# Train and test
+model_bert = LogisticRegression(max_iter=1000)
+model_bert.fit(X_train, y_train)
+y_bert = model_bert.predict(X_test)
+
+# Get results and print it
+acc_bert = accuracy_score(y_test, y_bert)
+report_bert = classification_report(y_test, y_bert)
+print("ClinicalBERT:")
+print("Accuracy:", acc_bert)
+print(report_bert)
+
+# Load TF-IDF features + labels
+X_tfidf = sparse.load_npz("tfidf_x.npz")
+y = np.load("y.npy", allow_pickle=True)
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X_tfidf, y, test_size=0.15, random_state=42, stratify=y
+)
+
+# Train and test
+model_tfidf = LogisticRegression(max_iter=1000)
+model_tfidf.fit(X_train, y_train)
+y_tfidf = model_tfidf.predict(X_test)
+
+# Get results and print it
+acc_tfidf = accuracy_score(y_test, y_tfidf)
+report_tfidf = classification_report(y_test, y_tfidf)
+print("TF-IDF:")
+print("Accuracy:", acc_tfidf)
+print(report_tfidf)
+
+# Get and visualize word weights
+corr = np.abs(model_tfidf.coef_).mean(axis=0)
+with open("tfidf_vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+feature_names = vectorizer.get_feature_names_out()
+
+df_corr = pd.DataFrame({
+    "word": feature_names,
+    "corr": corr
+}).sort_values("corr", ascending=False)
+
+print("\nTop predictive words:")
+print(df_corr.head(20))
+
+print("\nLeast useful words:")
+print(df_corr.tail(20))
+
+# measure variation across classes
+variation = np.std(model_tfidf.coef_, axis=0)
+
+df_var = pd.DataFrame({
+    "word": feature_names,
+    "variation": variation
+}).sort_values("variation")
+
+print("\nMost non-specific words:")
+print(df_var.head(20))
+
+print("\nMost class-specific words:")
+print(df_var.tail(20))
+
+
+# Export the results
+df_corr.to_csv("word_correlation.csv", index=False)
+df_var.to_csv("word_variation.csv", index=False)
+
+with open("results/metrics.txt", "w") as f:
+    f.write("ClinicalBERT\n")
+    f.write(f"Accuracy: {acc_bert}\n")
+    f.write(report_bert)
+
+    f.write("\n\nTF-IDF\n")
+    f.write(f"Accuracy: {acc_tfidf}\n")
+    f.write(report_tfidf)
+
+# Top and least predictive words
+top = df_corr.head(20)
+plt.figure()
+plt.barh(top["word"], top["corr"])
+plt.gca().invert_yaxis()
+plt.title("Top Correlated Words")
+plt.savefig("top_words.png", bbox_inches="tight")
+plt.close()
+
+bottom = df_corr.tail(20)
+plt.figure()
+plt.barh(bottom["word"], bottom["corr"])
+plt.gca().invert_yaxis()
+plt.title("Least Correlated Words")
+plt.savefig("least_words.png", bbox_inches="tight")
+plt.close()
+
+# Specificity of words
+spec = df_var.tail(20)
+plt.figure()
+plt.barh(spec["word"], spec["variation"])
+plt.gca().invert_yaxis()
+plt.title("Most Class-Specific Words")
+plt.savefig("specific_words.png", bbox_inches="tight")
+plt.close()
+
+nonspec = df_var.head(20)
+plt.figure()
+plt.barh(nonspec["word"], nonspec["variation"])
+plt.gca().invert_yaxis()
+plt.title("Most Non-Specific Words")
+plt.savefig("nonspecific_words.png", bbox_inches="tight")
+plt.close()
+
+# Histograms
+plt.figure()
+plt.hist(df_corr["corr"], bins=50)
+plt.title("Correlation Distribution")
+plt.savefig("correlation_dist.png", bbox_inches="tight")
+plt.close()
+
+plt.figure()
+plt.hist(df_var["variation"], bins=50)
+plt.title("Variation Distribution")
+plt.savefig("variation_dist.png", bbox_inches="tight")
+plt.close()
---
+++ feature_extraction.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from transformers import AutoTokenizer, AutoModel
+from scipy import sparse
+import numpy as np
+import pickle
+import torch
+
+# Use GPU + batch processing
+batch_size = 128
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+# Open the dataset
+df = pd.read_csv("data1.csv")
+
+# Parse the words into something usable
+def parse_list(s):
+    s = s.strip("[]")
+    return [w.strip().strip("'").strip('"') for w in s.split(",")]
+transcripts = df["parsed_transcript"].apply(parse_list).tolist()
+docs = ["".join(t) for t in transcripts]
+
+# TF-IDF vectorization
+vectorizer = TfidfVectorizer()
+X_tfidf = vectorizer.fit_transform(docs)
+
+# Save TF-IDF + vectorizer
+sparse.save_npz("tfidf_x.npz", X_tfidf)
+with open("tfidf_vectorizer.pkl", "wb") as f:
+    pickle.dump(vectorizer, f)
+
+
+# ClinicalBERT
+tokenizer = AutoTokenizer.from_pretrained("medicalai/ClinicalBERT")
+model = AutoModel.from_pretrained("medicalai/ClinicalBERT").to(device)
+embeddings = []
+
+# Process batches
+for i in range(0, len(docs), batch_size):
+    # get docs and update progress
+    batch = docs[i:i+batch_size]
+    print(i)
+
+    # Apply the tokenizer to prep the input and add them to the device
+    # We need them to be pytorch tensors
+    inputs = tokenizer(
+        batch,
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=512
+    )
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+
+    # We use .no_grad since we are not training, which speeds things up
+    # Then we call ClinicalBERT using the inputs
+    with torch.no_grad():
+      outputs = model(
+      input_ids=inputs["input_ids"],
+      attention_mask=inputs["attention_mask"]
+    )
+
+    # Move the embeddings out of the GPU if we used it
+    cls_embeddings = outputs.last_hidden_state[:,0,:]
+    embeddings.append(cls_embeddings.cpu())
+
+# Concatenate all of the batch embeddings into one
+X_bert = torch.cat(embeddings)
+
+# Export the result and the labels
+np.save("clinicalbert_x.npy", X_bert.cpu().numpy())
+y = df["specialty"].to_numpy()
+np.save("y.npy", y)
+
+
